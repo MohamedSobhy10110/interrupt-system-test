84548be814a6345a7d33b58b6ff8a78464fa7491
mohamed_sobhy <58358402+MohamedSobhy10110@users.noreply.github.com>
2020-03-30 16:26:09 +0200

    Add files via upload

---
+++ interrupt_test/interrupt_test/interrupt_test.c
@@ -0,0 +1,17 @@
+/*
+ * interrupt_test.c
+ *
+ * Created: 20/03/14 11:49:32 AM
+ *  Author: Mohamed Sobhy
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
